feae3dd3ad884c4ba568e6aea9d733f508b7d27c
Aaditya8C <aadityajp0419@gmail.com>
2025-02-17 18:24:31 +0530

    17_feb

--- base.cpp
+++ base.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 #define for0(i, n) for (int i = 0; i < (int)(n); ++i)            // 0 based indexing
-#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)           // 1 based indexing
+#define for1(i, n) for (int i = 1; i < (int)(n); ++i)            // 0 based indexing starts with 1
+#define for11(i, n) for (int i = 1; i <= (int)(n); ++i)          // 1 based indexing
 #define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
 #define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i)      // reverse 0 based.
 #define forr1(i, n) for (int i = (int)(n); i >= 1; --i)          // reverse 1 based
@@ -28,6 +29,9 @@
 // Avoiding wrap around of size()-1 where size is a unsigned int.
 #define sz(a) int((a).size())
 
+// Macro for ceiling division
+#define ceil_div(a, b) (((a) + ((b) - 1)) / (b))
+
 using namespace std;
 
 // Shorthand for commonly used types
@@ -40,17 +44,21 @@ typedef vector<ll> vll;
 typedef vector<vll> vvll;
 typedef double ld;
 
+void solve()
+{
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     cout.precision(10);
-    // cout << fixed;
 
     int T;
     cin >> T;
     while (T--)
     {
+        solve();
     }
     return 0;
 }
