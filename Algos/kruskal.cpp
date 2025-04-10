73faeceb4b0694ab30f5511e7998df29a6e4ded6
Aaditya8C <aadityajp0419@gmail.com>
2025-04-10 15:53:11 +0530

    cheetsheets_v2

--- Algos/kruskal.cpp
+++ Algos/kruskal.cpp
@@ -1,8 +1,14 @@
+// kruskal: builds Minimum Spanning Tree (MST) using Kruskal's algorithm
 struct DSU
 {
     vector<int> par;
+    // initialize DSU with n nodes
     DSU(int n) : par(n) { iota(par.begin(), par.end(), 0); }
+
+    // find with path compression
     int find(int x) { return x == par[x] ? x : par[x] = find(par[x]); }
+
+    // unite two sets, return false if already connected
     bool unite(int x, int y)
     {
         x = find(x);
@@ -13,13 +19,14 @@ struct DSU
         return true;
     }
 };
+
 int kruskal(int n, vector<tuple<int, int, int>> &edges)
 {
-    sort(edges.begin(), edges.end());
+    sort(edges.begin(), edges.end()); // sort edges by weight
     DSU dsu(n);
     int total = 0;
     for (auto [w, u, v] : edges)
-        if (dsu.unite(u, v))
-            total += w;
-    return total;
+        if (dsu.unite(u, v)) // if u and v are in different sets
+            total += w;      // include this edge in MST
+    return total;            // return total weight of MST
 }
